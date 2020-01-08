9527742aa3e2ded13451748b28feaaccb392f7e1
Adam <amoore@uat.edu>
2020-01-08 13:22:49 -0700

    Near end of Lecture 2.2

---
+++ Assets/Scripts/TankData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankData : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public float rotateSpeed = 180.0f;
+}
---
+++ Assets/Scripts/TankMotor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMotor : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Transform tf;
+    private TankData data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+        tf = gameObject.GetComponent<Transform>();
+        data = gameObject.GetComponent<TankData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(data.moveSpeed);
+        Rotate(data.rotateSpeed);
+    }
+
+    public void Move(float speed)
+    {
+        // Create a vector to hold our speed data
+        Vector3 speedVector = tf.forward * speed;
+
+        // Call SimpleMove() and send it our vector
+        // Note that SimpleMove() will apply Time.deltaTime, and convert to meters per second for us!
+        characterController.SimpleMove(speedVector);
+    }
+
+    public void Rotate(float speed)
+    {
+        // Create a vector to hold our rotation data
+        Vector3 rotateVector = Vector3.up * speed * Time.deltaTime;
+
+        // Now, rotate our tank by this value - we want to rotate in our local space (not in world space).
+        tf.Rotate(rotateVector, Space.Self);
+    }
+}
