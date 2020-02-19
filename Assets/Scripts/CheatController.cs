c6e19e4ed531af52dd3c3c1827f74feb3242297d
Adam <amoore@uat.edu>
2020-02-19 13:10:10 -0700

    Lecture 7.1 Completed

---
+++ Assets/Scripts/CheatController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(PowerupController))]
+public class CheatController : MonoBehaviour
+{
+    private PowerupController powCon;
+
+    public Powerup cheatPowerup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (powCon == null)
+        {
+            powCon = gameObject.GetComponent<PowerupController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            powCon.Add(cheatPowerup);
+        }
+    }
+}
